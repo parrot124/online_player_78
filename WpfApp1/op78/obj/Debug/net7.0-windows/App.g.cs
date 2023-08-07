18bbaba9e971de58f54f2b0ca608f39299200c74
famisom <komissarovadim@gmail.com>
2023-08-07 22:14:29 +0300

    Rename window
    Rename window

--- WpfApp1/op78/obj/Debug/net7.0-windows/App.g.cs
+++ WpfApp1/op78/obj/Debug/net7.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace WpfApp1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace WpfApp1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
         public static void Main() {
             WpfApp1.App app = new WpfApp1.App();
             app.InitializeComponent();
